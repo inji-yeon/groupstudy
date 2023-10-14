b0fc19efc5eb64b4f1834d71e51a2b85cee01293
inji-yeon <inji2349@gmail.com>
2023-10-14 15:29:16 +0900

    study

---
+++ backjoon/src/main/java/Step01/Rabbit.java
@@ -0,0 +1,17 @@
+package Step01;
+
+public class Rabbit {
+    public static void main(String[] args) {
+
+        System.out.println("　   /⌒ ヽ　  　/ ⌒ \\");
+        System.out.println("　  ｜   ｜|　　|｜  |");
+        System.out.println(" 　 ｜   ｜|  　|｜  |");
+        System.out.println(" 　 ｜   ｜|　　|｜  |");
+        System.out.println(" 　 ｜   ｜ -- ｜   |");
+        System.out.println(" 　 ｜ ／　  　　＼  |");
+        System.out.println("　  /　　　　　　    \\");
+        System.out.println("　 /　      　　     \\");
+        System.out.println(" 　(　  ●　　ο　　●　  )");
+        System.out.println("    /////      /////");
+    }
+}
